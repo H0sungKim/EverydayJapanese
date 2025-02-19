7229f1eed4f62253e0f30dd2052ff363c8bcc88a
Hosung.Kim <hosungkim5108@gmail.com>
2025-02-19 15:19:59 +0900

    apply refactor on widget

--- iOS/StudyWideget/AppIntent.swift
+++ iOS/StudyWideget/AppIntent.swift
@@ -50,33 +50,42 @@ enum StudyPartEnum: String, AppEnum {
         .jlptN1: "JLPT N1 단어",
     ]
     
-    func getFileName() -> String {
+    var fileName: String {
+        switch self {
+        case .kanjiElementary1, .kanjiElementary2, .kanjiElementary3, .kanjiElementary4, .kanjiElementary5, .kanjiElementary6, .kanjiMiddle:
+            return "kanji"
+            
+        case .jlptN5, .jlptN4, .jlptN3, .jlptN2, .jlptN1:
+            return "jlpt"
+        }
+    }
+    
+    var idRange: ClosedRange<Int> {
         switch self {
         case .kanjiElementary1:
-            return "kanji1"
+            return 0...79
         case .kanjiElementary2:
-            return "kanji2"
+            return 80...239
         case .kanjiElementary3:
-            return "kanji3"
+            return 240...439
         case .kanjiElementary4:
-            return "kanji4"
+            return 440...641
         case .kanjiElementary5:
-            return "kanji5"
+            return 642...834
         case .kanjiElementary6:
-            return "kanji6"
+            return 835...1025
         case .kanjiMiddle:
-            return "kanji7"
-            
+            return 1026...2135
         case .jlptN5:
-            return "n5"
+            return 0...717
         case .jlptN4:
-            return "n4"
+            return 718...1385
         case .jlptN3:
-            return "n3"
+            return 1386...3524
         case .jlptN2:
-            return "n2"
+            return 3525...5269
         case .jlptN1:
-            return "n1"
+            return 5270...7964
         }
     }
 }
