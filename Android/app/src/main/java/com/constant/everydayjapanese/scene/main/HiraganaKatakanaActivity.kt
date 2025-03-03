69c3e1bef2dfd7f23b6d9b1e3bf1dbdfe7fcdfb6
김기훈 <kimkihoon@gimgihun-ui-MacBookPro.local>
2025-03-03 17:06:31 +0900

    [AOS] add Hirakana grid

--- Android/app/src/main/java/com/constant/everydayjapanese/scene/main/HiraganaKatakanaActivity.kt
+++ Android/app/src/main/java/com/constant/everydayjapanese/scene/main/HiraganaKatakanaActivity.kt
@@ -1,8 +1,15 @@
 package com.constant.everydayjapanese.scene.main
 
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.constant.everydayjapanese.R
 import com.constant.everydayjapanese.databinding.ActivityHiraganaKatakanaBinding
+import com.constant.everydayjapanese.util.HHLog
 import com.constant.everydayjapanese.util.HHStyle
 import com.constant.everydayjapanese.util.IndexEnum
 import com.constant.everydayjapanese.util.nonNull
@@ -19,6 +26,60 @@ class HiraganaKatakanaActivity : AppCompatActivity() {
         var indexEnum: IndexEnum,
     )
 
+    interface OnSelectItemListener {
+        fun onSelectItem(position: Int)
+    }
+
+    inner class HiraganaKatakanaAdapter(private val context: Context) :
+        RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        // Public Inner Class, Struct, Enum, Interface
+        inner class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+            fun bind(position: Int) {
+            }
+        }
+
+
+        // Private Constant
+        private val item: Int = 0
+
+        // Public Variable
+        // Private Variable
+        private var onSelectItemListener: OnSelectItemListener? = null
+
+        // Override Method or Basic Method
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int,
+        ): RecyclerView.ViewHolder {
+            HHLog.d(TAG, "onCreateViewHolder()")
+            val view = LayoutInflater.from(context).inflate(R.layout.grid_hiragana_katakana, parent, false)
+            return ItemViewHolder(view)
+        }
+
+        override fun getItemCount(): Int = 0
+
+        override fun getItemViewType(position: Int): Int {
+            return item
+        }
+
+        override fun onBindViewHolder(
+            holder: RecyclerView.ViewHolder,
+            position: Int,
+        ) {
+            val itemViewHolder: ItemViewHolder = holder as ItemViewHolder
+            HHLog.d(TAG, "onBindViewHolder() position = $position")
+            itemViewHolder.bind(position)
+        }
+
+        // Public Method
+        fun setOnSelectItemListener(onSelectItemListener: OnSelectItemListener) {
+            this.onSelectItemListener = onSelectItemListener
+        }
+        // Public Method
+    } // end of AlphabetAdapter
+
+
     // companion object
     companion object {
         public val EXTRA_INDEX_ENUM = "EXTRA_INDEX_ENUM"
@@ -32,6 +93,7 @@ class HiraganaKatakanaActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHiraganaKatakanaBinding
 
     private lateinit var param: Param
+    private lateinit var hiraganaKatakanaAdapter: HiraganaKatakanaAdapter
 
     // Override Method or Basic Method
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -68,6 +130,8 @@ class HiraganaKatakanaActivity : AppCompatActivity() {
                 },
             )
 
+            hiraganaKatakanaAdapter = HiraganaKatakanaAdapter(this@HiraganaKatakanaActivity)
+            recyclerview.adapter = hiraganaKatakanaAdapter
         }
     }
 }
