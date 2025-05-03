81fabe5fb5483606e68bc22b5ad3f7ed355a52a4
kihoon.kim <kihoon740@gmail.com>
2025-05-03 20:08:12 +0900

    [AOS] version up

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.constant.everydayjapanese"
         minSdk = 28
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.00.09"
+        versionCode = 10
+        versionName = "1.00.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
